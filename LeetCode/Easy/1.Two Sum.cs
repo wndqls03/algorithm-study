24a5c8e0d869682783326e09fc54b3f47c24d4f0
wndqls03 <113694275+wndqls03@users.noreply.github.com>
2026-06-22 17:52:07 +0900

    Add files via upload

---
+++ LeetCode/Easy/1.Two Sum.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        /*for(int i = 0; i<nums.Length; i++)
+        {
+            for(int j = i+1; j<nums.Length; j++)
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    return new int [] {i,j};
+                }
+            }
+        }
+        return new int[0];*/
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int need = target - nums[i];
+            if (map.ContainsKey(need))
+            {
+                return new int[] { map[need], i };
+            }
+            map[nums[i]] = i;
+        }
+        return new int[0];
+    }
+}
