2dcab1fd5d32b8afd59f739316f4828cf814b534
wndqls03 <113694275+wndqls03@users.noreply.github.com>
2026-07-09 23:29:28 +1200

    Update Program.cs

--- LeetCode/Easy/Program.cs
+++ LeetCode/Easy/Program.cs
@@ -1,43 +1,33 @@
-﻿
-  
-  public class ListNode {
-      public int val;
-      public ListNode next;
-      public ListNode(int val=0, ListNode next=null) {
-         this.val = val;
-         this.next = next;
-     }
- }
- 
 public class Solution
 {
-    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    public bool IsValid(string s)
     {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
+        Stack<char> stack = new Stack<char>();
 
-        while (list1 != null && list2 != null)
+        for (int i = 0; i < s.Length; i++)
         {
-            if (list1.val < list2.val)
+            char c = s[i];
+
+            if (c == '(' || c == '[' || c == '{')
             {
-                current.next = list1;
-                list1 = list1.next;
+                stack.Push(c);
             }
             else
             {
-                current.next = list2;
-                list2 = list2.next;
+                if (stack.Count == 0)
+                    return false;
+
+                char top = stack.Pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{'))
+                {
+                    return false;
+                }
             }
-            current = current.next;
         }
-        if (list1 != null)
-        {
-            current.next = list1;
-        }
-        else
-        {
-            current.next = list2;
-        }
-        return dummy.next;
+
+        return stack.Count == 0;
     }
 }
