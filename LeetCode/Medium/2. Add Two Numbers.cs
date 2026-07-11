15de6b4e9130778e49e6b4d58a3f0e364148b72d
wndqls03 <113694275+wndqls03@users.noreply.github.com>
2026-07-11 22:35:15 +1200

    Add files via upload

---
+++ LeetCode/Medium/2. Add Two Numbers.cs
@@ -0,0 +1,42 @@
+﻿public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int sum = carry;
+            if (l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            int digit = sum % 10;
+
+            current.next = new ListNode(digit);
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+}
