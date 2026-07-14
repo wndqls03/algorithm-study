a14dbb6181127fc2fb3df5ccd87572c03f957f90
wndqls03 <113694275+wndqls03@users.noreply.github.com>
2026-07-14 22:06:40 +1200

    Add files via upload

---
+++ LeetCode/Medium/3. Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> map = new HashSet<char>();
+        int left = 0;
+        int result = 0;
+        for(int right = 0; right<s.Length; right++)
+        {
+            while (map.Contains(s[right]))
+            {
+                map.Remove(s[left]);
+                left++;
+            }
+            map.Add(s[right]);
+            result = Math.Max(result, right - left + 1);
+        }
+        return result;
+    }
+}
